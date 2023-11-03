33584951cd89361e25923e063da16c1f0b9fc2c6
neerajsingh1802 <139963133+neerajsingh1802@users.noreply.github.com>
2023-11-03 17:10:02 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+// let elm=document.getElementsByTagName("h2");
+// for(let i=0;i<elm.length;i++){
+//     elm[i].innerHTML="Hello by vedanshi and bambam";
+//     console.log(elm);
+
+// }
+
+// 2nd EXAMPLE 
+
+let tm=document.getElementById("raj");
+let elm=tm.getElementsByTagName("h2");
+for(let i=0; i<elm.length; i++){
+    elm[i].innerHTML="Hello by vedanshi and bambam and Tamu";
+}
+console.log(elm);
